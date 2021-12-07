4acbaeadc100ba88769f0bf8fa6b213d9f8626e4
Michael Chandler <macjr93@yahoo.com>
2021-12-07 00:26:03 -0800

    add basic protection against attacks

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const methodOverride = require('method-override');
 const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Methods Block
 const User = require('./models/user');
@@ -50,6 +51,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended:true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 // Session & Flash Settings
 const sessionConfig = {
@@ -75,7 +79,6 @@ passport.serializeUser(User.serializeUser());passport.deserializeUser(User.deser
 
 // Flash Message Middleware
 app.use((req, res, next) => {
-    // console.log(req.session);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
--- schemas.js
+++ schemas.js
@@ -1,19 +1,47 @@
-const Joi = require('Joi');
+// variable declarations
+const BaseJoi = require('Joi');
+const sanitizeHtml = require('sanitize-html');
 
+// escapeHtml function and extension declaration
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value });
+                return clean;
+            }
+        }
+    }
+});
+
+// joi extension declaration
+const Joi = BaseJoi.extend(extension);
+
+// campgrounds scema declaration
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
 
+// review schema declaration
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
 });
