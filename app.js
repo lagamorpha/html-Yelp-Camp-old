645202564ef2e8114d14177dee9c5fa8d16a3b9f
Michael Chandler <macjr93@yahoo.com>
2021-12-16 23:49:52 -0800

    add start script

--- app.js
+++ app.js
@@ -194,6 +194,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server Ready Route
-app.listen(3000, () => {
-    console.log('Server Active on Port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server Active on Port ${port}`);
 });
